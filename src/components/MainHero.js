35ca1d7dd8d9ae51984b0e419897e79cc2be4abe
eletrocp <brernolopes@hotmail.com>
2022-10-18 20:46:41 -0300

    :sparkles: feat: componente de conteudo principal

---
+++ src/components/MainHero.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const MainHero = () => {
+  return (
+    <main>
+      <h1>Driele Santos</h1>
+      <p>
+        Sou estudante de licenciatura em artes na UFBA trabalho na 
+        area de X sou apaixonada por Y tenho 20 anos moro em Salvador BA.
+      </p>
+      <div>
+        <button
+          type="button"
+        >
+          Meu Trabalho
+        </button>
+        <button
+          type="button"
+        >
+          Meu Curriculo
+        </button>
+      </div>
+      <div>
+        <img src="" alt="Minha foto"/>
+        <h3>Artista</h3>
+      </div>
+    </main>
+  );
+};
+
+export default MainHero;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import MainHero from '../components/MainHero';
 
 const MainPage = () => {
   return (
     <div>
       <Header />
+      <MainHero />
     </div>
   );
 };
