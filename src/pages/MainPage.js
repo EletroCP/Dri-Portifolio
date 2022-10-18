1e3bc3cefc8d071e645ba2f849c87d56301d000f
eletrocp <brernolopes@hotmail.com>
2022-10-18 20:53:54 -0300

    :recycle: refactor: incorporando o componente Works em MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import MainHero from '../components/MainHero';
+import Works from '../components/Works';
 
 const MainPage = () => {
   return (
     <div>
       <Header />
       <MainHero />
+      <Works />
     </div>
   );
 };
